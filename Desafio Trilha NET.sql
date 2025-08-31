7f0207e8a65ecd6c4c2d2226bc7ef01cd17e5c83
marcosribeiro-dev <marcosribeiro.dev@outlook.com>
2025-08-31 19:37:17 -0300

    Desafio SQL

---
+++ Desafio Trilha NET.sql
@@ -0,0 +1,81 @@
+--1
+SELECT
+	Nome,
+	Ano
+FROM Filmes
+
+--2
+SELECT 
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+ORDER BY Ano
+
+--3
+SELECT
+	Nome,
+	Ano,
+	Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro'
+
+--4
+SELECT *
+FROM Filmes
+WHERE Ano = '1997'
+
+--5
+SELECT *
+FROM Filmes
+WHERE Ano > '2000'
+
+--6
+SELECT *
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao
+
+--7
+SELECT 
+	Ano,
+	COUNT(Ano) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+--8
+SELECT * FROM Atores
+WHERE Genero = 'M'
+
+--9
+SELECT * FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+--10
+SELECT 
+	Nome,
+	Genero
+FROM Filmes
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+
+--11
+SELECT 
+	Nome,
+	Genero
+FROM Filmes
+INNER JOIN FilmesGenero ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Genero = 'Mistério'
+
+--12
+SELECT 
+	Nome,
+	PrimeiroNome,
+	UltimoNome,
+	Papel
+FROM Atores
+INNER JOIN  ElencoFilme ON Atores.Id = ElencoFilme.IdAtor
+INNER JOIN Filmes ON ElencoFilme.IdFilme = Filmes.Id
